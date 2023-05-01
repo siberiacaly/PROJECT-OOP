0be77b0d5848e7818be60c38d44a3839456651a6
Tomáš Calábek <124521380+siberiacaly@users.noreply.github.com>
2023-05-01 16:39:14 +0200

    Delete projekt directory

--- projekt/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PROJEKT_OOP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
